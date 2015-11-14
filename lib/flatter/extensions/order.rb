f58b4db1fb4dd3056fc637706c4a13a4bb873392
Artem Kuzko <a.kuzko@gmail.com>
2015-11-14 23:36:53 +0200

    Add order to mappings

--- lib/flatter/extensions/order.rb
+++ lib/flatter/extensions/order.rb
@@ -5,10 +5,21 @@ module Flatter
 
       register_as :order
 
+      mapping.add_option :index do
+        def index
+          options[:index] || 0
+        end
+      end
+
       mapper.add_option :index do
         def index
           options[:index] || 0
         end
+
+        def local_mappings
+          @_local_mappings ||= super.sort_by(&:index)
+        end
+        protected :local_mappings
 
         def mappers_chain(context)
           super.sort_by do |mapper|
